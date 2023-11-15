48fbbdc4408d44594b0c0bc937c10e7a96211daf
the-geniustech <holumidey70@gmail.com>
2023-11-15 14:43:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import CreateCustomer from "./features/customers/CreateCustomer";
+import Customer from "./features/customers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
+
+function App() {
+  const customer = useSelector((store) => store.customer);
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      {!customer.fullName || !customer.nationalID ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
